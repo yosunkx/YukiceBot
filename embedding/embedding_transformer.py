29fa92253bb7d7d1c2519a4e8712e48c67ee9b84
Kevin <Kevin@DESKTOP-5UMQ4SL>
2023-05-12 17:39:49 -0400

    fixes

--- embedding/embedding_transformer.py
+++ embedding/embedding_transformer.py
@@ -36,14 +36,12 @@ def tag_split_text(title, section_name, text):
     return tagged_texts
 
 
-# Mean Pooling - Take average of all tokens
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output.last_hidden_state
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
 
-# Encode text
 def encode(texts, title=None, section_name=None):
     texts = tag_split_text(title, section_name, texts)
 
