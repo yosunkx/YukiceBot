1bdf01ec938127e68da80ecf57f10ac00cde653d
Kevin <Kevin@DESKTOP-5UMQ4SL>
2023-05-17 01:08:26 -0400

    added requirements

---
+++ tests/test_embedding_healthcheck.py
@@ -0,0 +1,23 @@
+import requests
+import socket
+
+server_ip = '192.168.0.110'  # IP of your server
+port = '8000'
+# Get the IP address of the current machine
+current_ip = socket.gethostbyname(socket.gethostname())
+
+
+host = 'localhost'
+
+
+base_url = f"http://{host}:{port}"
+
+try:
+    response = requests.get(f"{base_url}/healthcheck")
+    response.raise_for_status()
+    health_status = response.json()
+except requests.exceptions.RequestException as e:
+    print(f"Could not connect to the server: {e}")
+    health_status = {"status": "server down"}
+
+print(f"Health status: {health_status}")
