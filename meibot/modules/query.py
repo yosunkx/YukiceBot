3f3c033c3f54bc4056c527c7b1909a5e024bed1b
Kevin <Kevin@DESKTOP-5UMQ4SL>
2023-06-10 23:07:19 -0400

    milvus connection

---
+++ meibot/modules/query.py
@@ -0,0 +1,49 @@
+from discord.ext import commands
+import discord
+import httpx
+import os
+import socket
+import asyncio
+from pymilvus import connections, exceptions, CollectionSchema, FieldSchema, DataType, Collection, utility
+
+server_ip = '192.168.0.133'
+local_host = 'localhost'
+milvus_port = '19530'
+milvus_alias = 'default'
+current_ip = socket.gethostbyname(socket.gethostname())
+
+# Get value of environment variable 'DOCKER'. If it doesn't exist, default to False.
+docker = os.getenv('DOCKER', False)
+
+milvus_host = os.getenv('EMBEDDING_HOST', server_ip if current_ip != server_ip else local_host)
+
+
+@commands.command()
+async def query(ctx, query_request: str = None):
+    if query_request is None:
+        await ctx.send("invalid request")
+        return
+    try:
+        connections.connect(
+            alias=milvus_alias,
+            host=milvus_host,
+            port=milvus_port
+        )
+        print(f"Connected to Milvus server at {milvus_host}:{milvus_port} with alias '{milvus_alias}'")
+
+    except Exception as e:
+        print(f"Failed to connect to Milvus server at {milvus_host}:{milvus_port}")
+        print(f"Error: {e}")
+
+    finally:
+        try:
+            connections.disconnect(milvus_alias)
+            print(f"Disconnected from Milvus server with alias '{milvus_alias}'")
+        except exceptions.ConnectionNotFoundError as e:
+            print(f"Failed to disconnect because no connection with alias '{milvus_alias}' was found")
+            print(f"Error: {e}")
+
+
+def setup(bot):
+    bot.add_command(query)
+
